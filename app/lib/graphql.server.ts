e71b496a57f4adce03b098a1d8a255d511cbe3b5
Michael Ward <mward82@gmail.com>
2022-05-17 02:11:56 +0100

    Fixes typescript errors

--- app/lib/graphql.server.ts
+++ app/lib/graphql.server.ts
@@ -2,6 +2,9 @@ import { GraphQLClient } from 'graphql-request'
 
 import { getSdk } from '~/generated/graphql.server'
 
+declare var HASURA_ENDPOINT: string | undefined
+declare var HASURA_TOKEN: string | undefined
+
 export const sdk = getSdk(
   new GraphQLClient(HASURA_ENDPOINT || '', {
     headers: {
