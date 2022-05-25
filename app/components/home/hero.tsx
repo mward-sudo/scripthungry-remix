04b7924ffc8ba53e050619eb1941d02fea6d9fe1
Michael Ward <mward82@gmail.com>
2022-05-25 22:57:56 +0000

    Update landing page Fixes #70

--- app/components/home/hero.tsx
+++ app/components/home/hero.tsx
@@ -1,34 +1,25 @@
-import { motion } from 'framer-motion'
-
-import { fadeInRight } from '~/lib/animations'
-
 export const Hero = () => {
   return (
-    <>
-      <svg className='absolute h-0 w-0'>
-        <clipPath id='cloud-clip-path' clipPathUnits='objectBoundingBox'>
-          <path d='M0.839,1 C0.928,1,1,0.882,1,0.736 C1,0.611,0.947,0.506,0.875,0.479 C0.874,0.214,0.742,0,0.58,0 C0.468,0,0.371,0.101,0.321,0.25 C0.305,0.239,0.289,0.233,0.271,0.233 C0.207,0.233,0.154,0.311,0.146,0.412 C0.063,0.439,0,0.559,0,0.703 C0,0.867,0.081,1,0.181,1 C0.209,1,0.82,1,0.839,1'></path>
-        </clipPath>
-      </svg>
-      <div className='hero flex flex-col gap-8 bg-base-200 bg-transparent p-8 xl:flex-row'>
-        <motion.div
-          className='hero-content h-[200px] w-[400px] flex-col object-cover p-0 drop-shadow-lg xl:flex-row'
-          style={{ clipPath: 'url(#cloud-clip-path)' }}
-          variants={fadeInRight}
-        >
-          <video autoPlay muted loop playsInline>
-            <source src='https://res.cloudinary.com/scripthungry-com-prog/video/upload/ac_none,c_scale,e_accelerate:-10,q_auto,vc_auto,f_auto,w_500/v1651437759/mixkit-flight-above-the-cloud-level-loop-video-32991-medium_upevqf.mp4' />
-          </video>
-        </motion.div>
-        <div className='flex-1 bg-gradient-to-b from-orange-400 to-orange-600 bg-clip-text text-center'>
-          <h1 className='m-0 mb-6 text-5xl font-bold text-transparent md:text-6xl lg:text-7xl 2xl:text-8xl'>
-            <div className='mb-2 text-6xl md:text-7xl lg:text-8xl 2xl:text-9xl'>
-              Super fast
-            </div>
-            <div>cloud web sites</div>
-          </h1>
+    <div className='p-8 px-2 text-center lg:px-16'>
+      <div className='flex flex-row gap-2 text-2xl sm:text-3xl md:text-4xl'>
+        <div className='flex flex-1 flex-col justify-center'>
+          <div className='mt-[.45em] h-[2px] flex-grow-0 bg-gradient-to-bl from-yellow-700 via-yellow-300 to-yellow-700'></div>
+        </div>
+        <h1 className='m-0 inline-block font-brand leading-none'>
+          <span className='bg-gradient-to-bl from-yellow-700 via-yellow-300 to-yellow-700 bg-clip-text font-normal text-transparent'>
+            scriptHungry
+          </span>
+        </h1>
+        <div className='flex flex-1 flex-col justify-center'>
+          <div className='mt-[.45em] h-[2px] flex-grow-0 bg-gradient-to-bl from-yellow-700 via-yellow-300 to-yellow-700'></div>
         </div>
       </div>
-    </>
+
+      <h2 className='m-0 inline-block pt-2 text-3xl sm:pt-4 sm:text-4xl md:text-5xl'>
+        <span className='bg-gradient-to-bl from-yellow-700 via-yellow-300 to-yellow-700 bg-clip-text font-normal text-transparent'>
+          super fast cloud web sites
+        </span>
+      </h2>
+    </div>
   )
 }
--- app/components/navigation/nav.tsx
+++ app/components/navigation/nav.tsx
@@ -16,7 +16,7 @@ export const Nav = ({ navLinks }: { navLinks: Graphcms_NavigationLink[] }) => {
       className='sticky top-4 mb-8 w-full text-base-content'
     >
       <div className='navbar rounded-box min-h-fit overflow-hidden border-2 border-transparent border-b-neutral/20 border-opacity-50 bg-gradient-to-b from-base-300/80 to-base-300/60 py-0 pl-4 pr-0 backdrop-blur-md'>
-        <div className='mr-2 flex-1 px-2'>
+        <div className='mr-2 flex-1 px-2 font-brand'>
           <Link to='/'>scriptHungry</Link>
         </div>
         <div className='flex-none md:hidden'>
