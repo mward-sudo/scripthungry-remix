0f4567e43a25674142cbbca1167c17912c143bc4
Michael Ward <mward82@gmail.com>
2022-06-11 15:37:53 +0000

    Implement currency conversion

---
+++ app/lib/currency.server.ts
@@ -0,0 +1,49 @@
+type ConvertApiResponse = {
+  motd: {
+    msg: string
+    url: string
+  }
+  success: boolean
+  query: {
+    from: string
+    to: string
+    amount: string
+  }
+  info: { rate: string }
+  historical: boolean
+  date: string
+  result: string
+}
+
+export type CurrencyConversionResult = {
+  from: string
+  to: string
+  fromAmount: string
+  toAmount: string
+}
+
+export const convertCurrency = async (
+  value: string,
+  from: string,
+  to: string,
+): Promise<CurrencyConversionResult> => {
+  const url = `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${value}&places=2`
+  const request = new Request(url)
+  const response = await fetch(request)
+  const data: ConvertApiResponse = await response.json()
+
+  return {
+    from: data.query.from,
+    to: data.query.to,
+    fromAmount: convertToCurrencyString(data.query.amount),
+    toAmount: convertToCurrencyString(data.result),
+  }
+}
+
+const convertToCurrencyString = (value: string, locale = 'en-GB') => {
+  const valueAsNumber = Number(value)
+  return new Intl.NumberFormat(locale, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(valueAsNumber)
+}
--- app/routes/currency/index.tsx
+++ app/routes/currency/index.tsx
@@ -1,7 +1,87 @@
+import { Form, useLoaderData } from '@remix-run/react'
+import type { LoaderFunction } from '@remix-run/server-runtime'
+import { json } from '@remix-run/server-runtime'
+import { BsCashCoin } from 'react-icons/bs'
+import { HiSwitchHorizontal } from 'react-icons/hi'
+
+import type { CurrencyConversionResult } from '~/lib/currency.server'
+import { convertCurrency } from '~/lib/currency.server'
+
+type LoaderData = {
+  currencyConversion: CurrencyConversionResult
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url)
+  const from = url.searchParams.get('from') ?? 'GBP'
+  const to = url.searchParams.get('to') ?? 'USD'
+  const fromAmount = url.searchParams.get('fromAmount') ?? '1.00'
+
+  const currencyConversion = await convertCurrency(fromAmount, from, to)
+
+  const data: LoaderData = {
+    currencyConversion,
+  }
+
+  return json(data)
+}
+
 const CurrencyConverterRoute = () => {
+  const { currencyConversion } = useLoaderData<LoaderData>()
+  const { from, to, fromAmount, toAmount } = currencyConversion
+
   return (
     <div>
-      <p>Currency converter</p>
+      <div className='text-center text-7xl mt-8 hidden md:block'>
+        <BsCashCoin className='inline-block' />
+      </div>
+      <Form className='form-control w-full not-prose items-center mt-8 flex-col md:flex-row gap-2 justify-center'>
+        <label className='input-group input-group-lg w-fit'>
+          <span>Amount</span>
+          <input
+            type='text'
+            name='fromAmount'
+            defaultValue={fromAmount}
+            className='input input-bordered focus:outline-none'
+          />
+        </label>
+        <label className='input-group input-group-lg w-fit'>
+          <span>from</span>
+          <select
+            name='from'
+            defaultValue={from}
+            className='select select-bordered focus:outline-none'
+          >
+            <option value='GBP'>GBP</option>
+            <option value='USD'>USD</option>
+            <option value='EUR'>EUR</option>
+          </select>
+        </label>
+        <label className='input-group input-group-lg w-fit'>
+          <span>to</span>
+          <select
+            name='to'
+            defaultValue={to}
+            className='select select-bordered focus:outline-none'
+          >
+            <option value='GBP'>GBP</option>
+            <option value='USD'>USD</option>
+            <option value='EUR'>EUR</option>
+          </select>
+        </label>
+        <button type='submit' className='btn btn-primary ml-4'>
+          <HiSwitchHorizontal className='mr-2' /> Convert
+        </button>
+      </Form>
+      <p className='text-5xl md:text-7xl text-center'>
+        <span className='text-purple-400 font-bold'>
+          {fromAmount} {from}
+        </span>{' '}
+        ={' '}
+        <span className='text-green-500 font-bold'>
+          {toAmount} {to}
+        </span>
+      </p>
     </div>
   )
 }
